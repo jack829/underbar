eeb6e31bfd74bc513263cfe56c966e9875b4c664
John Peterson <jlpeterson829@gmail.com>
2015-01-08 11:23:24 -0800

    _.filter code

--- src/underbar.js
+++ src/underbar.js
@@ -86,12 +86,20 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+  	var results= [];
+  	_.each(collection, function(item) {
+  		if (test(item)) {
+  			results.push(item);
+  		}
+  	});
+  	return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    
   };
 
   // Produce a duplicate-free version of the array.
